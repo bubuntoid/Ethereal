db41668869ecb5411344c24aef9c40fc375e1e36
Dmitrii Stafeev <bubuntoid@gmail.com>
2021-07-27 18:06:58 -0700

    add DownloadThumbnailRequestDto

---
+++ src/BE/YOVPS.WebAPI/Controllers/MainController/Models/DownloadThumbnailRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace YOVPS.WebAPI.Controllers.MainController.Models
+{
+    public class DownloadThumbnailRequestDto
+    {
+        public string Url { get; set; }
+
+        public class Validator : AbstractValidator<DownloadThumbnailRequestDto>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.Url)
+                    .NotEmpty();
+            }
+        }
+    }
+}
