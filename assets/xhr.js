712389a0b367e6171fef5a67430b82040449e842
webgtx <dxv1d@tuta.io>
2021-07-29 20:03:30 +0300

    Added webPlayer with XMLHttpRequest

--- assets/xhr.js
+++ assets/xhr.js
@@ -1,14 +1,36 @@
-const reqURL = "http://81.177.135.200:322/api/download/zip"
+const reqURL = 'http://81.177.135.200:322/api/chapters'
 const xhr = new XMLHttpRequest()
 
 function sendReq(method, url, body = null) {
   return new Promise((resolve, reject) => {
-    xhr.open(method, url)
-    xhr.responseType = 'json'
-    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.open(method, url);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onload = () => {
       xhr.status >= 400 ?  reject('invalid request url') : resolve(xhr.response);
     }
-    xhr.send(JSON.stringify(body))
+    xhr.send(JSON.stringify(body));
+  })
+  .catch(err => {
+    loaderChecker(false)
+    mainInput.value = '';
+    mainInput.setAttribute('placeholder', err);
+    webPlayer.style.transition = '1s ease';
+    webPlayer.style.transform = 'translate(100vw, 0)';
+  })
+  .then(data => {
+    loaderChecker(false)
+    chapters = data;
+    if (data) {
+      webPlayer.style.transition = '1s ease';
+      webPlayer.style.transform = 'translate(0, 0)';
+      playerSubTitle.innerHTML = chapters[0].name;
+      let itemsList = '';
+      chapters.forEach((item, idx) => {
+        itemsList += `<a class="nav__link" href="#">${chapters[idx].duration}</a> \n`;        
+      });
+      console.log(itemsList);
+      sidebarNav.innerHTML = itemsList;
+    }
   })
 }
--- src/FE/js/main.js
+++ src/FE/js/main.js
@@ -1,12 +1,32 @@
 const footer = document.querySelector('.footer');
 const mainInput = document.querySelector('.main__input-text');
+const webPlayer = document.querySelector('.wplayer')
+const playerTitle = document.querySelector('.player__title')
+const playerSubTitle = document.querySelector('.player__subtitle')
+const sidebarNav = document.querySelector('.sidebar__nav')
+
+const loader = document.querySelector('.loader')
+const chapters = {}
+
 
 let footerChecker = (isOpen = false) => {
   footer.style.transition = '1s ease';
   isOpen ? footer.style.transform = 'translate(0, -30vh)' : footer.style.transform = 'translate(0, 0vh)';
 }
 
+function loaderChecker(isValid = false) {
+  loader.style.transition = '1s ease'
+  if (isValid) {
+    loader.style.opacity = '1' 
+    loader.style.zIndex = '1'
+  } else {
+    loader.style.opacity = '0'
+    loader.style.zIndex = '0'
+  }
+}
+
 function input2req(url) {
+  loaderChecker(true)
   url = mainInput.value
   console.log(url)
   sendReq('POST', reqURL, {
