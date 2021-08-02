e2ec60b8a467476fe4b3eac2efbfd2e6792c08f1
webgtx <dxv1d@tuta.io>
2021-08-02 22:16:56 +0300

    Writed worked WebPlayer, added download feature.

---
+++ dist/xhr.js
@@ -0,0 +1,79 @@
+const reqUrlChapters = 'http://81.177.135.200:322/api/chapters'
+const xhr = new XMLHttpRequest()
+
+function sendReq(method, url, responseType, body = null) {
+  return new Promise((resolve, reject) => {
+    xhr.open(method, url);
+    xhr.responseType = responseType;
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = () => {
+      xhr.status >= 400 ? reject('invalid request url') : resolve(xhr.response);
+    }
+    xhr.send(JSON.stringify(body));
+  })
+  .catch(err => {
+    loaderChecker(false)
+    mainInput.value = '';
+    mainInput.setAttribute('placeholder', err);
+    webPlayer.style.transition = '1s ease';
+    webPlayer.style.transform = 'translate(100vw, 0)';
+  })
+}
+
+function getChapters(url) {
+    sendReq("POST", reqUrlChapters, 'json', {
+    url: url,
+    includeThumbnails: true
+  })
+  .then(data => {
+    loaderChecker(false)
+    const chapters = data;
+    if (data) {
+      const playerBg = document.querySelector('.player__bg');
+
+      webPlayer.style.transition = '1s ease';
+      webPlayer.style.transform = 'translate(0, 0)';
+
+      playerSubTitle.innerHTML = chapters[0].name;
+      playerBg.setAttribute('src', `data:image/png;base64, ${chapters[0].thumbnailBase64}`)
+
+      let itemsList = '';
+      chapters.forEach((item, idx) => {
+        itemsList += `
+        <li class="nav-list-i">
+          <a class="nav__link" id="${idx}" href="#">${chapters[idx].original}</a>
+        </li>
+        `;
+      });
+      navList.innerHTML = itemsList;
+      const navLinks = document.querySelectorAll('.nav__link');
+      console.log(navLinks);
+      navLinks.forEach((item, idx) => {
+        item.addEventListener('click', () => {
+          playerSubTitle.innerHTML = chapters[idx].name;
+          playerBg.setAttribute('src', `data:image/png;base64, ${chapters[idx].thumbnailBase64}`)
+        })
+      })
+    }
+  })
+}
+function downloadZip(url, description = null) {
+  loaderChecker(true);
+  sendReq("POST", "http://81.177.135.200:322/api/download/zip", "blob", {
+    url: url,
+    description: description
+  })
+  .catch(err => loaderChecker(false))
+  .then(data =>{
+    loaderChecker(false);
+    console.log(data);
+      let tempEl = document.createElement("a");
+      	document.body.appendChild(tempEl);
+    	tempEl.style = "display: none";
+        url = window.URL.createObjectURL(data);
+        tempEl.href = url;
+        tempEl.download = 'file.zip';
+        tempEl.click();
+		window.URL.revokeObjectURL(url);
+  })
+}
--- src/FE/js/main.js
+++ src/FE/js/main.js
@@ -4,21 +4,20 @@ const webPlayer = document.querySelector('.wplayer');
 const playerTitle = document.querySelector('.player__title');
 const playerSubTitle = document.querySelector('.player__subtitle');
 const navList = document.querySelector('.nav-list');
-const navLinks = ''
 const loader = document.querySelector('.loader');
-const chapters = {};
+const downloadBtn = document.querySelector('.download__button');
 
 
 let footerChecker = (isOpen = false) => {
   footer.style.transition = '1s ease';
-  isOpen ? footer.style.transform = 'translate(0, -30vh)' : footer.style.transform = 'translate(0, 0vh)';
+  isOpen ? footer.style.transform = 'translate(0, -30%)' : footer.style.transform = 'translate(0, 0%)';
 }
 
 function loaderChecker(isValid = false) {
   loader.style.transition = '1s ease'
   if (isValid) {
     loader.style.opacity = '1' 
-    loader.style.zIndex = '1'
+    loader.style.zIndex = '6'
   } else {
     loader.style.opacity = '0'
     loader.style.zIndex = '0'
@@ -29,8 +28,5 @@ function input2req(url) {
   loaderChecker(true)
   url = mainInput.value
   console.log(url)
-  sendReq('POST', reqURL, {
-    url: url
-  })
+  getChapters(url);
 }
-
