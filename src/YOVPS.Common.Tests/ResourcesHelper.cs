d6e9ccec961fb74f389714fd037f405dd761f19e
Dmitrii Stafeev <bubuntoid@gmail.com>
2021-08-13 16:26:01 +0300

    ffmpeg tests

---
+++ src/YOVPS.Common.Tests/ResourcesHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace YOVPS.Common.Tests
+{
+    public static class ResourcesHelper
+    {
+        public static string GetResourceFullPath(string name)
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), "Resources", name);
+        }
+    }
+}
--- src/YOVPS.Core.UnitTests/FfmpegWrapperTests.cs
+++ src/YOVPS.Core.UnitTests/FfmpegWrapperTests.cs
@@ -1,15 +1,48 @@
 ﻿using System.IO;
+using System.Threading.Tasks;
 using NUnit.Framework;
+using Xabe.FFmpeg.Downloader;
+using YOVPS.Common.Tests;
 
 namespace YOVPS.Core.UnitTests
 {
     [TestFixture]
     public class FfmpegWrapperTests
     {
+        private string videoPath; 
+        
         [SetUp]
-        public void SetUp()
+        public async Task SetUp()
         {
-            FfmpegWrapper.ExecutablesPath = Path.Combine(Directory.GetCurrentDirectory(), "ffmpeg", "ffmpeg.exe");
+            var ffmpegPath = Path.Combine(Directory.GetCurrentDirectory(), "ffmpeg");
+            await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Full, ffmpegPath);
+
+            FfmpegWrapper.ExecutablesPath = ffmpegPath;
+            videoPath = ResourcesHelper.GetResourceFullPath("sample.mp4");
+        }
+
+        [Test]
+        public void SaveTrimmedAsync_VideoSaved()
+        {
+            
+        }
+        
+        [Test]
+        public void SaveTrimmedAsync_VideoHasCorrectLength()
+        {
+            
+        }
+        
+        [Test]
+        public void SaveTrimmedAsync_VideoHasAudioTrack()
+        {
+            
+        }
+        
+        [Test]
+        public void SaveImageAsync_ImageSaved()
+        {
+            
         }
     }
 }
