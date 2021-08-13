1a15cde597425522f9af807daaededb58a560969
Dmitrii Stafeev <bubuntoid@gmail.com>
2021-08-13 22:36:17 +0300

    ffmpeg tests

--- src/YOVPS.Core.UnitTests/FfmpegWrapperTests.cs
+++ src/YOVPS.Core.UnitTests/FfmpegWrapperTests.cs
@@ -1,6 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Xabe.FFmpeg;
 using Xabe.FFmpeg.Downloader;
 using YOVPS.Common.Tests;
 
@@ -9,22 +12,45 @@ namespace YOVPS.Core.UnitTests
     [TestFixture]
     public class FfmpegWrapperTests
     {
-        private string videoPath; 
+        private string videoPath;
+        private string tempOutputPath;
+        private VideoChapter chapter;
         
         [SetUp]
         public async Task SetUp()
         {
             var ffmpegPath = Path.Combine(Directory.GetCurrentDirectory(), "ffmpeg");
             await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Full, ffmpegPath);
-
-            FfmpegWrapper.ExecutablesPath = ffmpegPath;
+            FfmpegWrapper.ExecutablesPath = Path.Combine(ffmpegPath, "ffmpeg");
+            FFmpeg.SetExecutablesPath(ffmpegPath);
+            
             videoPath = ResourcesHelper.GetResourceFullPath("sample.mp4");
+            tempOutputPath = Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tempOutputPath);
+            
+            chapter = new VideoChapter
+            {
+                StartTimespan = TimeSpan.FromMinutes(5),
+                EndTimespan = TimeSpan.FromMinutes(10),
+            };
         }
 
-        [Test]
-        public void SaveTrimmedAsync_VideoSaved()
+        [TearDown]
+        public void TearDown()
         {
+            Directory.Delete(tempOutputPath, true);
+        }
+        
+        [Test]
+        public async Task SaveTrimmedAsync_VideoSaved()
+        {
+            var outputPath = Path.Combine(tempOutputPath, $"{Guid.NewGuid()}.mp3");
+            await FfmpegWrapper.SaveTrimmedAsync(videoPath, outputPath, chapter, 0, 1);
             
+            var mediaInfo = await FFmpeg.GetMediaInfo(outputPath);
+            var videoDuration = mediaInfo.VideoStreams.First().Duration;
+
+            Assert.That(videoDuration, Is.EqualTo(chapter.Duration));
         }
         
         [Test]
