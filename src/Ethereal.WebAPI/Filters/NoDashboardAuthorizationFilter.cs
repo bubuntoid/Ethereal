0dfd13a6c3c547d2797a3e7eecb473cee664f1c9
Dmitriy Stafeev <51204805+bubuntoid@users.noreply.github.com>
2022-04-02 19:48:21 +0400

    upgrade hangfire; use hangfireDashboard; workerCount = 20 (#59)
    * upgrade hangfire; use hangfireDashboard; workerCount = 20
    
    * cleanup

---
+++ src/Ethereal.WebAPI/Filters/NoDashboardAuthorizationFilter.cs
@@ -0,0 +1,12 @@
+﻿using Hangfire.Dashboard;
+
+namespace Ethereal.WebAPI.Filters
+{
+    public class NoDashboardAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
