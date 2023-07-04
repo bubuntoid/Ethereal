53b4e803f7741d3d67be4ffa1c32d648bbb53d9e
EMRD95 <114953576+EMRD95@users.noreply.github.com>
2023-07-04 20:29:40 +0000

    Add the thumbnail as image metadata for tracks
    Uses the thumbnail as metadata images/album covers for the tracks with error handling.

--- src/Ethereal.Application/Commands/ConvertVideoCommand.cs
+++ src/Ethereal.Application/Commands/ConvertVideoCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Ethereal.Application.Exceptions;
@@ -7,6 +8,7 @@ using Ethereal.Application.ProcessingJobLogger;
 using Ethereal.Domain;
 using Ethereal.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using TagLib;
 
 namespace Ethereal.Application.Commands;
 
@@ -36,15 +38,45 @@ public class ConvertVideoCommand
         job.Status = ProcessingJobStatus.Processing;
         await dbContext.SaveChangesAsync();
 
-        for (var i = 0; i < chapters.Count; i++)
-        {
-            var chapter = chapters.ElementAt(i);
+for (var i = 0; i < chapters.Count; i++)
+{
+    var chapter = chapters.ElementAt(i);
 
-            await job.LogAsync($"Converting video [{i + 1}/{chapters.Count}] ({chapter.Name})");
+    await job.LogAsync($"Converting video [{i + 1}/{chapters.Count}] ({chapter.Name})");
 
-            await ffmpegWrapper.SaveTrimmedAsync(job.GetLocalVideoPath(settings), job.GetChapterLocalFilePath(chapter),
-                chapter);
-        }
+    await ffmpegWrapper.SaveTrimmedAsync(job.GetLocalVideoPath(settings), job.GetChapterLocalFilePath(chapter), chapter);
+
+	// Add metadata to the split file
+	var filePath = job.GetChapterLocalFilePath(chapter);
+	var file = TagLib.File.Create(filePath);
+	file.Tag.Title = chapter.Name;
+	file.Tag.Album = job.Video.Title;  // Or whatever you want the album to be
+	file.Tag.Comment = "Split with Ethereal";
+
+	// Add the thumbnail as album art
+	var thumbnailPath = System.IO.Path.Combine(settings.TempPath, job.Id.ToString(), settings.ThumbnailsDirectoryName, i + ".png");  // Use the chapter number as the file name
+
+	if (System.IO.File.Exists(thumbnailPath))
+	{
+	    var picture = new TagLib.Picture(thumbnailPath);
+	    file.Tag.Pictures = new TagLib.IPicture[] { picture };
+	}
+	else
+	{
+	    // If the specific thumbnail doesn't exist, try to use a different one
+	    var thumbnailDirectory = System.IO.Path.Combine(settings.TempPath, job.Id.ToString(), settings.ThumbnailsDirectoryName);
+	    var thumbnails = System.IO.Directory.GetFiles(thumbnailDirectory, "*.png");
+	    if (thumbnails.Length > 0)
+	    {
+		var picture = new TagLib.Picture(thumbnails[0]);  // Use the first available thumbnail
+		file.Tag.Pictures = new TagLib.IPicture[] { picture };
+	    }
+	    // If no thumbnails are available, don't set the album art
+	}
+
+	file.Save();
+
+	}
 
         await dbContext.SaveChangesAsync();
         await job.LogAsync("Converting succeeded");
