4f8f86fbee8c1a02a05eba4158f15b5a8afe9b30
EMRD95 <114953576+EMRD95@users.noreply.github.com>
2023-07-09 19:43:15 +0200

    Fix issue with some timestamps description & Adding metadata to the tracks plus some small bug fix (#66)
    * Fix issue with some timestamps description
    
    * 4 characters timestamp cases
    
    Handle cases where the timestamp is only 4 characters and not 5.
    
    Issue noted on this video https://www.youtube.com/watch?v=D1zR9DkYgu4
    
    1 · Pulse of the Night: 00:00
    2 · Motivation Monday: 2:31
    3 · Sunrise Surprise: 5:37
    4 · The Jam: 7:56
    5 · Astral: 11:11
    6 · Neon Rainbow Popcorn: 13:54
    
    * 4 characters timestamps issue
    
    Handle cases where the timestamp is only 4 characters and not 5.
    
    Issue noted on this video https://www.youtube.com/watch?v=D1zR9DkYgu4
    
    1 · Pulse of the Night: 00:00
    2 · Motivation Monday: 2:31
    3 · Sunrise Surprise: 5:37
    4 · The Jam: 7:56
    5 · Astral: 11:11
    6 · Neon Rainbow Popcorn: 13:54
    
    * Added TagLib to Ethereal.Application.csproj
    
    * Add the thumbnail as image metadata for tracks
    
    Uses the thumbnail as metadata images/album covers for the tracks with error handling.

--- src/Ethereal.Application/Extensions/TimeSpanExtensions.cs
+++ src/Ethereal.Application/Extensions/TimeSpanExtensions.cs
@@ -4,25 +4,35 @@ namespace Ethereal.Application.Extensions;
 
 public static class TimeSpanExtensions
 {
-    public static bool TryParseToTimeSpan(this string str, out TimeSpan timespan)
+public static bool TryParseToTimeSpan(this string str, out TimeSpan timespan)
+{
+    // Input formats:
+    // 0:00
+    // 00:00
+    // 00:00:00
+    
+    if (str.Length == 4) // m:ss
     {
-        // Input formats:
-        // 0:00
-        // 00:00
-        // 00:00:00
-
-        if (str.Length == 5) // 00:00
-        {
-            var parts = str.Split(':');
-            timespan = new TimeSpan(
-                0, // hours
-                int.Parse(parts[0]), // minutes
-                int.Parse(parts[1])); // seconds
-            return true;
-        }
-
-        return TimeSpan.TryParse(str, out timespan);
+        var parts = str.Split(':');
+        timespan = new TimeSpan(
+            0, // hours
+            int.Parse(parts[0]), // minutes
+            int.Parse(parts[1])); // seconds
+        return true;
     }
+    else if (str.Length == 5) // 00:00
+    {
+        var parts = str.Split(':');
+        timespan = new TimeSpan(
+            0, // hours
+            int.Parse(parts[0]), // minutes
+            int.Parse(parts[1])); // seconds
+        return true;
+    }
+
+    return TimeSpan.TryParse(str, out timespan);
+}
+
 
     public static TimeSpan ClearMilliseconds(this TimeSpan time)
     {
